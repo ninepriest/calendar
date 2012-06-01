390fc76f52b74c39b193584c4bb280db4df20450
Chekmarev Sergey <schekmarev@crystalspring.kz>
2012-06-01 16:14:56 +0600

    version 0.2
    + Внешний вид

--- gen/kz/nine/calendar/R.java
+++ gen/kz/nine/calendar/R.java
@@ -12,18 +12,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int myborder=0x7f020001;
     }
     public static final class id {
-        public static final int row0=0x7f050000;
-        public static final int row1=0x7f050001;
-        public static final int row2=0x7f050002;
-        public static final int row3=0x7f050003;
-        public static final int row4=0x7f050004;
-        public static final int row5=0x7f050005;
-        public static final int row6=0x7f050006;
+        public static final int bmonth=0x7f050000;
+        public static final int row0=0x7f050001;
+        public static final int row1=0x7f050002;
+        public static final int row2=0x7f050003;
+        public static final int row3=0x7f050004;
+        public static final int row4=0x7f050005;
+        public static final int row5=0x7f050006;
+        public static final int row6=0x7f050007;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int calendar=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/kz/nine/calendar/MyCalendar.java
+++ src/kz/nine/calendar/MyCalendar.java
@@ -5,9 +5,17 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TableRow;
@@ -17,31 +25,47 @@ public class MyCalendar extends Activity {
 	GregorianCalendar gc;
 	LinearLayout lay;
 	int month,day;
-	String weekdays[] = {"пн","вт","ср","чт","пт","сб","вс"};
+	String weekdays[] = {"пн","вт","ср","чт","пт","сб","вс"},
+			monthe[]={"Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октыбрь","Ноябрь","Декабрь"};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.calendar);
         
         gc = new GregorianCalendar();
-        month = gc.get(Calendar.MONTH)+1;
+        month = gc.get(Calendar.MONTH);
+        
+        Button b = (Button)findViewById(R.id.bmonth);
+        b.setBackgroundColor(Color.TRANSPARENT);
+        b.setTextColor(Color.WHITE);
+        b.setTextSize(24);
+        b.setText(monthe[month]+" "+gc.get(Calendar.YEAR));
+        
+        
         day = gc.get(Calendar.DAY_OF_YEAR);
+        
+      //  gc.get(Calendar.)
         
         TableRow tr = (TableRow)findViewById(R.id.row0);
         lay = new LinearLayout(getApplicationContext());
         for (int i = 0; i<=6; i++){
         	LinearLayout l = new LinearLayout(lay.getContext());
         	LayoutParams lp = new LayoutParams(63,LayoutParams.WRAP_CONTENT);
+        	l.setBackgroundDrawable(getApplicationContext().getResources().getDrawable(R.drawable.myborder));
         	l.setPadding(10, 10, 10, 10);
         	lp.setMargins(3, 0, 3, 3);
         	l.setLayoutParams(lp);
-        	l.setBackgroundColor(Color.WHITE);
+        	//l.setBackgroundColor(Color.LTGRAY);
         	l.getBackground().setAlpha(125);
         	TextView text = new TextView(lay.getContext());
+        	LayoutParams lpt = new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT);
         	text.setText(weekdays[i]);
         	text.setTextSize(23);
         	text.setTextColor(Color.WHITE);
+        	text.setGravity(Gravity.CENTER);
+        	text.setLayoutParams(lpt);
          	l.addView(text);        	
         	lay.addView(l);
         }
@@ -50,6 +74,7 @@ public class MyCalendar extends Activity {
         
         System.out.println("Calendar Day Of Year = "+gc.get(Calendar.DAY_OF_YEAR));
         
+        gc.set(Calendar.DAY_OF_MONTH, 1);
         if (gc.get(Calendar.DAY_OF_WEEK)!=2){
         	gc.set(Calendar.DAY_OF_YEAR, gc.get(Calendar.DAY_OF_YEAR)-gc.get(Calendar.DAY_OF_WEEK)+2);
         }
@@ -75,31 +100,66 @@ public class MyCalendar extends Activity {
         	lay = new LinearLayout(getApplicationContext());
         	
         	for (int j=1; j<=7; j++){
+        		//Button ib = new Button(lay.getContext());
             	LinearLayout l = new LinearLayout(lay.getContext());
             	LayoutParams lp = new LayoutParams(63,LayoutParams.WRAP_CONTENT);
             	LayoutParams lpt = new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT);
-            	l.setPadding(13, 13, 13, 13);
             	lp.setMargins(3, 3, 3, 3);
             	l.setLayoutParams(lp);
-            	l.setBackgroundColor(Color.WHITE);
+            	l.setBackgroundColor(Color.GRAY);
             	TextView text = new TextView(lay.getContext());
             	String t=""+gc.get(Calendar.DAY_OF_MONTH);
             	text.setText(t);
             	text.setTextSize(21);
-            	text.setTextColor(Color.WHITE);
+            	text.setTextColor(Color.LTGRAY);
             	text.setLayoutParams(lpt);
             	text.setGravity(Gravity.CENTER);
             	l.getBackground().setAlpha(88);
-//            	if ((gc.get(Calendar.DAY_OF_YEAR)!=day)&&(gc.get(Calendar.MONTH)!=month)){
-//            	l.getBackground().setAlpha(88);} else {l.getBackground().setAlpha(185);}
             	
-            	if (gc.get(Calendar.MONTH)+1!=month){
-            		text.setTextColor(Color.GRAY);
+            	if (gc.get(Calendar.MONTH)!=month){
+            		text.setTextColor(Color.DKGRAY);
             	}
             	if (gc.get(Calendar.DAY_OF_YEAR)==day){
-            		l.getBackground().setAlpha(185);
+            		text.setTextColor(Color.WHITE);
+            		l.setBackgroundDrawable(getApplicationContext().getResources().getDrawable(R.drawable.myborder));
+            		text.setTypeface(null, Typeface.BOLD);
+            		l.getBackground().setAlpha(125);
             	}
-             	l.addView(text);        	
+            	l.setPadding(13, 13, 13, 13);
+             	l.addView(text);       
+             	l.setOnTouchListener(new OnTouchListener() {
+					
+					@Override
+					public boolean onTouch(View v, MotionEvent event) {
+						// TODO Auto-generated method stub
+						
+						switch (event.getAction()){
+						case MotionEvent.ACTION_DOWN : {
+							LinearLayout l = ((LinearLayout) v);
+							l.setBackgroundColor(Color.WHITE);
+							l.getBackground().setAlpha(150);
+							System.out.println("ACTION_DOWN");
+							}
+						break;
+						case MotionEvent.ACTION_UP : {
+							LinearLayout l = ((LinearLayout) v);
+							l.setBackgroundColor(Color.WHITE);
+							l.getBackground().setAlpha(88);
+							System.out.println("ACTION_UP");
+						}
+						case MotionEvent.ACTION_CANCEL : {
+							LinearLayout l = ((LinearLayout) v);
+							l.setBackgroundColor(Color.WHITE);
+							l.getBackground().setAlpha(88);
+							System.out.println("ACTION_CANCEL");}
+						break;
+						case MotionEvent.ACTION_MOVE : {
+							System.out.println("ACTION_MOVE");}
+						break;
+						}
+						return false;
+					}
+				});
             	lay.addView(l);
             	gc.add(Calendar.DAY_OF_YEAR, 1);
         	}
